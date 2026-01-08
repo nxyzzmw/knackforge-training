0f8718bef9d703582ce3bc0e2ab7df4f4e0857fa
Nayeem <mohamednayeemullah@knackforge.com>
2026-01-08 18:20:25 +0530

    practiced more react

--- React - practice/my-react-app/vite.config.js
+++ React - practice/my-react-app/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/kf-training/React%20-%20practice/my-react-app/",
   plugins: [react()],
 })
