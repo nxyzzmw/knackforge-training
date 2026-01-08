476db06ed3699c30287cfce2f4eea501f0336fe9
Nayeem <mohamednayeemullah@knackforge.com>
2026-01-08 18:26:52 +0530

    updated react folder

--- React - practice/my-react-app/vite.config.js
+++ React - practice/my-react-app/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/kf-training/React%20-%20practice/my-react-app/",
   plugins: [react()],
 })
--- React/my-react-app/src/components/List.jsx
+++
@@ -1,9 +0,0 @@
-export default function List({ items }) {
-  return (
-    <ul className="list">
-      {items.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </ul>
-  );
-}
